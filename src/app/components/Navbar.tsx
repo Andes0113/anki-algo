1adbc154d39e94899a823ba65a9b752789b67e41
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2024-07-06 17:54:42 -0400

    Basic UI

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="w-full flex justify-between items-center">
+      <div className="text-xl ml-4 py-4 px-2">AnkiAlgo</div>
+      <div className="text-lg flex justify-end gap-2 p-1 mr-6">
+        <div>Hello</div>
+        <div>goodbye</div>
+      </div>
+    </div>
+  );
+}
